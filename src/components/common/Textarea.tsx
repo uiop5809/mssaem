57e74d027968bb7f8f63f4153680ea9ed4cb3b8b
parkyejin <uiop5809@naver.com>
2024-07-25 01:15:05 +0900

    test(#5): Textarea 공통 컴포넌트

---
+++ src/components/common/Textarea.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+const TextareaTheme = {
+  color: {
+    gray: 'text-gray2',
+  },
+  size: {
+    large: 'px-7.5 py-5 text-headline', // 이용약관
+  },
+}
+
+type Color = keyof typeof TextareaTheme.color
+type Size = keyof typeof TextareaTheme.size
+
+export interface TextareaProps {
+  value: string
+  color: Color
+  size: Size
+}
+
+const Textarea = ({ value, color, size }: TextareaProps) => (
+  <textarea
+    className={`bg-white border-gray6 border-2 rounded-3.75 w-full ${TextareaTheme.color[color]} ${TextareaTheme.size[size]} transparent-scrollbar resize-none`}
+    value={value}
+    readOnly
+    onFocus={(e) => e.target.blur()}
+  />
+)
+
+export default Textarea
