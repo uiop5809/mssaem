9eab5d9426c3a94670a2d1d591ef9daf7e5e6604
parkyejin <uiop5809@naver.com>
2024-07-30 13:24:31 +0900

    design(#5): Home Three Hot 컴포넌트 Hot 추가

--- src/components/home/Hot.tsx
+++ src/components/home/Hot.tsx
@@ -17,34 +17,33 @@ const Hot = ({
   worryBoardId,
   worryBoardTitle,
 }: HotProps) => {
-  let currentLabel
-  if (boardId != null) {
-    currentLabel = '게시물'
-  } else if (discussionId != null) {
-    currentLabel = '토론글'
-  } else if (worryBoardId != null) {
-    currentLabel = '고민글'
+  const getCurrentLabel = () => {
+    if (boardId != null) return '게시물'
+    if (discussionId != null) return '토론글'
+    if (worryBoardId != null) return '고민글'
+    return '내용 없음'
   }
 
+  const getTitle = () => {
+    if (boardId != null) return boardTitle
+    if (discussionId != null) return discussionTitle
+    if (worryBoardId != null) return worryBoardTitle
+    return null
+  }
+
+  const currentLabel = getCurrentLabel()
+  const title = getTitle()
+
   return (
-    <div className="flex flex-col justify-between w-full h-44 p-9 bg-white rounded-7.5">
+    <div className="relative flex flex-col justify-between w-full h-44 pt-8.75 pr-7.5 pb-5 pl-7.5 bg-white rounded-7.5">
+      <div className="absolute top-[-10px] left-3.75 bg-alarm text-white text-footnote px-2.5 py-2 font-regular">
+        HOT
+      </div>
       <div className="text-gray2 text-headline font-semibold">
         지금의 {currentLabel}
       </div>
-      {boardId != null && (
-        <div className="text-maindark text-title3 font-semibold">
-          {boardTitle}
-        </div>
-      )}
-      {discussionId != null && (
-        <div className="text-maindark text-title3 font-semibold">
-          {discussionTitle}
-        </div>
-      )}
-      {worryBoardId != null && (
-        <div className="text-maindark text-title3 font-semibold">
-          {worryBoardTitle}
-        </div>
+      {title && (
+        <div className="text-maindark text-title3 font-semibold">{title}</div>
       )}
       <div className="text-gray2 text-caption underline text-right">
         바로가기
