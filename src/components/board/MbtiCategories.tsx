8f0ef9c80bfb4ff8aedb764ad54ba3c1c81040bb
parkyejin <uiop5809@naver.com>
2024-08-06 23:40:34 +0900

    feat(#7): mbti별 게시물 개수

--- src/components/board/MbtiCategories.tsx
+++ src/components/board/MbtiCategories.tsx
@@ -2,43 +2,51 @@
 
 import Image from 'next/image'
 import mbtiList from '@/constants/mbtiList'
+import { useBoardListNumber } from '@/service/board/useBoardService'
 
 interface MbtiCategoriesProps {
   selectedMbti: string
   setMbti: (mbti: string) => void
 }
 
-// TODO: 게시물 개수 수정
-const MbtiCategories = ({ selectedMbti, setMbti }: MbtiCategoriesProps) => (
-  <div className="grid grid-cols-5 gap-4">
-    <div
-      className={`col-span-1 flex items-start justify-center cursor-pointer ${selectedMbti === 'all' ? 'underline' : ''}`}
-      onClick={() => setMbti('all')}
-    >
-      전체 (5,230)
+const MbtiCategories = ({ selectedMbti, setMbti }: MbtiCategoriesProps) => {
+  const { data } = useBoardListNumber()
+  const totalBoardCount = data?.boardCount || 0
+
+  return (
+    <div className="grid grid-cols-5 gap-4">
+      <div
+        className={`col-span-1 flex items-start justify-center cursor-pointer ${selectedMbti === 'all' ? 'underline' : ''}`}
+        onClick={() => setMbti('all')}
+      >
+        전체 ({totalBoardCount})
+      </div>
+      <div className="col-span-4 grid grid-cols-4 gap-4">
+        {mbtiList.map((mbti, index) => {
+          const mbtiCount = (data as any)?.[mbti.toLowerCase()] || 0
+          return (
+            <div
+              key={index}
+              className="flex gap-3 items-center cursor-pointer"
+              onClick={() => setMbti(mbti)}
+            >
+              <p
+                className={`w-21 text-gray2 ${selectedMbti === mbti ? 'underline' : ''}`}
+              >
+                {mbti} ({mbtiCount})
+              </p>
+              <Image
+                src="/images/board/star_empty.svg"
+                alt="star"
+                width={16}
+                height={16}
+              />
+            </div>
+          )
+        })}
+      </div>
     </div>
-    <div className="col-span-4 grid grid-cols-4 gap-4">
-      {mbtiList.map((mbti, index) => (
-        <div
-          key={index}
-          className="flex gap-3 items-center cursor-pointer"
-          onClick={() => setMbti(mbti)}
-        >
-          <p
-            className={`w-21 text-gray2 ${selectedMbti === mbti ? 'underline' : ''}`}
-          >
-            {mbti} (210)
-          </p>
-          <Image
-            src="/images/board/star_empty.svg"
-            alt="star"
-            width={16}
-            height={16}
-          />
-        </div>
-      ))}
-    </div>
-  </div>
-)
+  )
+}
 
 export default MbtiCategories
