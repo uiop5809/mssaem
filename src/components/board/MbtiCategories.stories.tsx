b8e33c5dfff9a14dda65e0a71f093e8aa697d30a
parkyejin <uiop5809@naver.com>
2024-08-09 00:48:22 +0900

    fix: MbtiCategories storybook 해결

--- src/components/board/MbtiCategories.stories.tsx
+++ src/components/board/MbtiCategories.stories.tsx
@@ -1,25 +1,17 @@
 import { Meta, StoryFn } from '@storybook/react'
-import MbtiCategories from './MbtiCategories'
+import MbtiCategories, { MbtiCategoriesProps } from './MbtiCategories'
 
 export default {
   title: 'Board/MbtiCategories',
   component: MbtiCategories,
 } as Meta
 
-const Template: StoryFn = (args) => <MbtiCategories {...args} />
+const Template: StoryFn<MbtiCategoriesProps> = (args) => (
+  <MbtiCategories {...args} />
+)
 
 export const Primary = Template.bind({})
 Primary.args = {
-  title: '카페에서 남친이랑 싸웠어',
-  content: '내가 말을 “만약에"라고 시작하면 너무 기빨린대',
-  imgUrl: '/images/common/thumbnail.svg',
-  likeCount: 30,
-  commentCount: 18,
-  createdAt: '24.07.25',
-  memberSimpleInfo: {
-    profileImgUrl: '/images/common/default.svg',
-    nickName: '유보라',
-    mbti: 'enfp',
-    badge: '엠비티어른',
-  },
+  selectedMbti: 'ISTJ',
+  setMbti: () => {},
 }
