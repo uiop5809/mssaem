57c9a72213d699386156d62d202ec7cfc8b6b83e
uiop5809 <uiop5809@naver.com>
2024-08-20 14:46:07 +0000

    refactor: HotThreeBox server side

--- src/components/home/HotThreeBox.tsx
+++ src/components/home/HotThreeBox.tsx
@@ -2,6 +2,7 @@
 
 import { useHotThree } from '@/service/home/useHomeService'
 import { User } from '@/model/User'
+import { HotThreeI } from '@/model/Home'
 import HotThree from './HotThree'
 import NotLogin from '../auth/NotLogin'
 import Login from '../auth/Login'
@@ -10,6 +11,14 @@ interface HotThreeBoxProps {
   userInfo: User | null
 }
 
+const renderHotThree = (hotThree: HotThreeI) => (
+  <>
+    <HotThree hotThree={hotThree} board="board" />
+    <HotThree hotThree={hotThree} board="discussion" />
+    <HotThree hotThree={hotThree} board="worry" />
+  </>
+)
+
 const HotThreeBox = ({ userInfo }: HotThreeBoxProps) => {
   const { data: hotThree } = useHotThree()
 
@@ -17,55 +26,14 @@ const HotThreeBox = ({ userInfo }: HotThreeBoxProps) => {
     <>
       {/* mobile */}
       <div className="bg-main4 flex items-center w-full-vw ml-half-vw gap-5 px-6% py-5 overflow-x-scroll scrollbar-hide sm:hidden">
-        {userInfo ? (
-          <>
-            <Login user={userInfo} />
-            {hotThree && (
-              <>
-                <HotThree hotThree={hotThree} board="board" />
-                <HotThree hotThree={hotThree} board="discussion" />
-                <HotThree hotThree={hotThree} board="worry" />
-              </>
-            )}
-          </>
-        ) : (
-          <>
-            <NotLogin />
-            {hotThree && (
-              <>
-                <HotThree hotThree={hotThree} board="board" />
-                <HotThree hotThree={hotThree} board="discussion" />
-                <HotThree hotThree={hotThree} board="worry" />
-              </>
-            )}
-          </>
-        )}
+        {userInfo ? <Login user={userInfo} /> : <NotLogin />}
+        {hotThree && renderHotThree(hotThree)}
       </div>
+
       {/* desktop */}
       <div className="hidden bg-main4 w-full-vw ml-half-vw gap-7.5 px-6% py-7.5 sm:px-8% sm:grid sm:grid-cols-2 md:px-13% lg:py-12.5 lg:grid lg:grid-cols-4">
-        {userInfo ? (
-          <>
-            {hotThree && (
-              <>
-                <HotThree hotThree={hotThree} board="board" />
-                <HotThree hotThree={hotThree} board="discussion" />
-                <HotThree hotThree={hotThree} board="worry" />
-              </>
-            )}
-            <Login user={userInfo} />
-          </>
-        ) : (
-          <>
-            <NotLogin />
-            {hotThree && (
-              <>
-                <HotThree hotThree={hotThree} board="board" />
-                <HotThree hotThree={hotThree} board="discussion" />
-                <HotThree hotThree={hotThree} board="worry" />
-              </>
-            )}
-          </>
-        )}
+        {hotThree && renderHotThree(hotThree)}
+        {userInfo ? <Login user={userInfo} /> : <NotLogin />}
       </div>
     </>
   )
