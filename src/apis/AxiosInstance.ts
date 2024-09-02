f45ddeb3ffd4a73f40754b509528cf61ebc882da
uiop5809 <uiop5809@naver.com>
2024-09-02 15:56:56 +0000

    fix: 카카오 로그인시 header Authorization

--- src/apis/AxiosInstance.ts
+++ src/apis/AxiosInstance.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios'
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
@@ -12,10 +12,20 @@ class Service {
         'Content-Type': 'application/json',
       },
     })
-  }
 
-  setAuthorizationHeader(token: string) {
-    this.http.defaults.headers.common.Authorization = `${token}`
+    // 요청 인터셉터를 사용하여 매 요청마다 토큰을 동적으로 설정
+    this.http.interceptors.request.use(
+      (config: InternalAxiosRequestConfig) => {
+        const token = localStorage.getItem('access_token')
+        if (token && config.headers) {
+          config.headers.set('Authorization', `${token}`)
+        }
+        return config
+      },
+      (error) => {
+        return Promise.reject(error)
+      },
+    )
   }
 }
 
--- src/app/kakao/login/page.tsx
+++ src/app/kakao/login/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Service from '@/apis/AxiosInstance'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
@@ -8,7 +7,6 @@ import { useEffect } from 'react'
 const KakaoLogin = () => {
   const router = useRouter()
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
-  const service = new Service()
 
   useEffect(() => {
     const getToken = async () => {
@@ -32,7 +30,6 @@ const KakaoLogin = () => {
         } else if (res.data.accessToken) {
           // 정상적으로 토큰을 받은 경우 /로 리다이렉트
           localStorage.setItem('access_token', res.data.accessToken)
-          service.setAuthorizationHeader(res.data.accessToken) // 생성된 서비스 인스턴스의 헤더 설정
           router.push('/')
         } else {
           // 토큰이 없을 경우에 /signin/terms로 리다이렉트
