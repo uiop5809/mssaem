14462cc4f8e0b5b5699b1ab1f23abc153f6caea6
uiop5809 <uiop5809@naver.com>
2024-09-02 17:26:12 +0000

    fix: 새로고침 시에도 userInfo

--- src/recoil/UserInfo.tsx
+++ src/recoil/UserInfo.tsx
@@ -1,7 +1,11 @@
 import { User } from '@/model/User'
 import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist()
 
 export const userInfoState = atom<User | null>({
   key: 'userInfoState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 })
