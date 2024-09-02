5b6d812d6a29b5a7e90b7a1df6379cd9c6dbc109
uiop5809 <uiop5809@naver.com>
2024-09-02 16:59:46 +0000

    fix: 로그인 새로고침 에러

--- src/recoil/Recoil.tsx
+++ src/recoil/Recoil.tsx
@@ -1,14 +1,32 @@
 'use client'
 
 import { ReactNode } from 'react'
-import { RecoilRoot } from 'recoil'
+import { RecoilRoot, MutableSnapshot } from 'recoil'
+import { userInfoState } from '@/recoil/UserInfo'
 
 interface RecoilRootProps {
   children: ReactNode
+  initialUserInfo?: any
 }
 
-const Recoil = ({ children }: RecoilRootProps) => {
-  return <RecoilRoot>{children}</RecoilRoot>
+const initializeState = (snapshot: MutableSnapshot, initialUserInfo?: any) => {
+  if (initialUserInfo) {
+    snapshot.set(userInfoState, initialUserInfo)
+  }
+}
+
+const Recoil = ({ children, initialUserInfo }: RecoilRootProps) => {
+  return (
+    <RecoilRoot
+      initializeState={(snapshot) => initializeState(snapshot, initialUserInfo)}
+    >
+      {children}
+    </RecoilRoot>
+  )
+}
+
+Recoil.defaultProps = {
+  initialUserInfo: null,
 }
 
 export default Recoil
